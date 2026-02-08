7f75f169f6cac3aa85bbf2be3ae07c31b6ef9f52
JR Tashjian <hello@jrtashjian.com>
2026-02-08 14:53:29 -0500

    Refactor PHPUnit tests

---
+++ phpunit/unit/BaseTestCase.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Base test case for PluginWP tests.
+ *
+ * @package PluginWP
+ */
+
+namespace PluginWP\Tests\Unit;
+
+use WP_Mock\Tools\TestCase as WP_Mock_TestCase;
+
+/**
+ * Base test case extending WP_Mock TestCase.
+ */
+class BaseTestCase extends WP_Mock_TestCase {
+}
